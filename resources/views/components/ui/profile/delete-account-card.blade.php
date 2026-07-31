a0cd2ba50fbc5024f3a39ad22cbd4d18f7f155ce
André Moreira <afrmscb@gmail.com>
2026-07-31 11:17:48 +0100

    fix profile

--- resources/views/components/ui/profile/delete-account-card.blade.php
+++ resources/views/components/ui/profile/delete-account-card.blade.php
@@ -16,7 +16,7 @@ Profile Delete Account Card Component
     icon='<svg class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"/></svg>'
 >
     <div x-data="{ open: false }">
-        <x-ui.button
+        <x-ui.buttons.button
             type="button"
             @click="open = true"
             variant="danger"
@@ -24,7 +24,7 @@ Profile Delete Account Card Component
             weight="semibold"
         >
             {{ __('Eliminar Conta') }}
-        </x-ui.button>
+        </x-ui.buttons.button>
 
         <div
             x-show="open"
